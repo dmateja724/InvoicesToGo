39ccbc0219970f1ed352b05ffb9c859989681dd0
Derrick Mateja <dmateja724@gmail.com>
2021-12-20 21:24:52 -0600

    added newInvoiceController and nav

--- InvoicesToGo/Controllers/Invoices/InvoicesController.swift
+++ InvoicesToGo/Controllers/Invoices/InvoicesController.swift
@@ -8,9 +8,41 @@
 import UIKit
 
 class InvoicesController: UIViewController {
+    // MARK: - Properties
+
+    @IBOutlet var tableView: UITableView!
+
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configure()
+    }
+
+    // MARK: - Helpers
+
+    func configure() {
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+
+    @IBAction func pressedCreateInvoiceButton(_ sender: Any) {
+        navigationController?.pushViewController(NewInvoiceController(), animated: true)
     }
 }
+
+// MARK: - UITableViewDataSource
+
+extension InvoicesController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
+    }
+}
+
+// MARK: - UITableViewDelegate
+
+extension InvoicesController: UITableViewDelegate {}
---
+++ InvoicesToGo/Controllers/NewInvoice/NewInvoiceController.swift
@@ -0,0 +1,16 @@
+//
+//  NewInvoiceController.swift
+//  InvoicesToGo
+//
+//  Created by Derrick Mateja on 12/20/21.
+//
+
+import UIKit
+
+class NewInvoiceController: UIViewController {
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
