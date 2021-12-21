c39a2c0c8c4ce50806fecf3a51e17c06059c7d2c
Derrick Mateja <dmateja724@gmail.com>
2021-12-20 21:37:45 -0600

    setup newInvoiceController

--- InvoicesToGo/Controllers/NewInvoice/NewInvoiceController.swift
+++ InvoicesToGo/Controllers/NewInvoice/NewInvoiceController.swift
@@ -8,9 +8,45 @@
 import UIKit
 
 class NewInvoiceController: UIViewController {
+    // MARK: - Properties
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var invoiceNumberLabel: UILabel!
+    
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configure()
+    }
+    
+    //MARK: - Helpers
+    
+    func configure() {
+        self.navigationItem.title = "New Invoice"
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 }
+
+// MARK: - UITableViewDataSource
+
+extension NewInvoiceController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        25
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let label = UILabel(frame: CGRect(x: 10, y: 0, width: tableView.frame.width, height: 50))
+        label.text = "item\(indexPath.item)"
+        cell.addSubview(label)
+        
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+
+extension NewInvoiceController: UITableViewDelegate {}
